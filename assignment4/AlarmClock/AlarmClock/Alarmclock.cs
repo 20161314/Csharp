c55d95f75ce84b426da69ea1cbf344c908921571
Riptide65536 <311234714@qq.com>
2025-03-20 17:04:11 +0800

    Homework 4

---
+++ assignment4/AlarmClock/AlarmClock/Alarmclock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AlarmClock
+{
+    public class AlarmClock
+    {
+        private readonly System.Timers.Timer _timer;
+        private DateTime _alarmTime;
+        private bool _alarmTriggered;
+
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+
+        public AlarmClock(DateTime alarmTime)
+        {
+            _alarmTime = alarmTime;
+            _timer = new System.Timers.Timer(1000); // 1秒间隔
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.AutoReset = true;
+            _timer.Start();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // 检查是否到达设定时间
+            // 若还没到，那么tick即可
+            // 如果超过了，就一直响直到用户回应
+            if (DateTime.Now >= _alarmTime)
+            {
+                Alarm?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                Tick?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ assignment4/GList/GList/GernericList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GList
+{
+    class GenericList<T>
+    {
+        public Node<T>? Head { get; set; }
+        private Node<T>? tail;
+        public GenericList()
+        {
+            tail = Head = null;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                Head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = Head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+}
